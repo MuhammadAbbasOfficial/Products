253594fbde65f0ff71a830e8aed19e2b5711362e
Muhammad Abbas <mabbas786official@gmail.com>
2025-12-06 15:41:22 +0500

    url changes

--- src/app.module.ts
+++ src/app.module.ts
@@ -11,7 +11,7 @@ import { MongooseModule } from '@nestjs/mongoose';
     ConfigModule.forRoot({
       isGlobal: true,
     }),
-    MongooseModule.forRoot("mongodb+srv://mabbas786official_db_user:v847J09yLkEkOQlg@khan.rsgmlgd.mongodb.net/?appName=khan")
+    MongooseModule.forRoot(process.env.MONGO_DB_URI!),
   ],
   controllers: [AppController],
   providers: [AppService],
