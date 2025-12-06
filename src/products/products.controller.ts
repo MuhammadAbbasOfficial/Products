b4a30543892d1827edf3fed96f7ad17c69e7f726
Muhammad Abbas <mabbas786official@gmail.com>
2025-12-06 13:39:09 +0500

    add products

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -1,4 +1,17 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
 
 @Controller('products')
-export class ProductsController {}
+export class ProductsController {
+
+
+
+    @Get()
+    getAllProducts() {
+        return [
+            { id: 1, name: 'Product A', price: 100 },
+            { id: 2, name: 'Product B', price: 150 },
+        ];
+    }
+
+
+}
