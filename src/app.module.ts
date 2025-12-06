665410558f5c14faf0a3f89bba6e702b906411de
Muhammad Abbas <mabbas786official@gmail.com>
2025-12-06 13:36:24 +0500

    Fix MongoDB connection and add ConfigModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,18 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ProductsModule } from './products/products.module';
+import { ConfigModule } from '@nestjs/config';  
+import { MongooseModule } from '@nestjs/mongoose';
+
 
 @Module({
-  imports: [],
+  imports: [ProductsModule,
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    MongooseModule.forRoot("mongodb+srv://mabbas786official_db_user:v847J09yLkEkOQlg@khan.rsgmlgd.mongodb.net/?appName=khan")
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
